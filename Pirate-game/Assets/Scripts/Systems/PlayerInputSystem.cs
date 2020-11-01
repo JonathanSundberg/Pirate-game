e2e3c421c7d04cff1c8e427497cef054ff62f251
Jonathan Sundberg <guitaraddict95@hotmail.com>
2020-11-01 12:54:40 +0100

    rearranged camera and limited movement

--- Pirate-game/Assets/Scripts/Systems/PlayerInputSystem.cs
+++ Pirate-game/Assets/Scripts/Systems/PlayerInputSystem.cs
@@ -1,22 +1,41 @@
 ﻿using Unity.Entities;
 using UnityEngine;
+using Unity.Transforms;
 using System;
 
 public class PlayerInputSystem : SystemBase
 {
     protected override void OnUpdate()
     {
-        Entities.ForEach((ref PlayerComponent playerComponent) =>
+        Entities.ForEach((ref PlayerComponent playerComponent, ref Translation pos) =>
         {
-            bool isRightKeyPressed = Input.GetKey(playerComponent.right);
-            bool isLeftKeyPressed = Input.GetKey(playerComponent.left);
-            bool isUpKeyPressed = Input.GetKey(playerComponent.up);
-            bool isDownKeyPressed = Input.GetKey(playerComponent.down);
+            bool isLeftKeyPressed   = false;
+            bool isUpKeyPressed     = false;
+            bool isDownKeyPressed   = false;
+            bool isRightKeyPressed  = false;
 
-            playerComponent.direction.x = Convert.ToInt32(isRightKeyPressed);
-            playerComponent.direction.x -= Convert.ToInt32(isLeftKeyPressed);
-            playerComponent.direction.z = Convert.ToInt32(isUpKeyPressed);
-            playerComponent.direction.z -= Convert.ToInt32(isDownKeyPressed);
+
+            if(pos.Value.x <= playerComponent.max_move_x)
+            {
+                isDownKeyPressed   = Input.GetKey(playerComponent.down);
+            }
+            if (pos.Value.x >= playerComponent.min_move_x)
+            {
+                isUpKeyPressed     = Input.GetKey(playerComponent.up);
+            }
+            if (pos.Value.z >= playerComponent.min_move_z)
+            {
+                isLeftKeyPressed = Input.GetKey(playerComponent.left);
+            }
+            if(pos.Value.z <= playerComponent.max_move_z)
+            {
+                isRightKeyPressed  = Input.GetKey(playerComponent.right);
+            }
+
+            playerComponent.direction.x = Convert.ToInt32(isDownKeyPressed);
+            playerComponent.direction.x -= Convert.ToInt32(isUpKeyPressed);
+            playerComponent.direction.z = Convert.ToInt32(isRightKeyPressed);
+            playerComponent.direction.z -= Convert.ToInt32(isLeftKeyPressed);
 
         }).Run();
     }
